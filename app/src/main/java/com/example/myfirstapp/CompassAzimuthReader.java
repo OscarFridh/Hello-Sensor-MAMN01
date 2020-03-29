3caf2563d5286434a37b1e928277cd7a2fcc57cb
Oscar Fridh <oscar.fridh.mkk@gmail.com>
2020-03-29 12:16:12 +0200

    Added low pass filter for accelerometer and magnetometer sensors

--- app/src/main/java/com/example/myfirstapp/CompassAzimuthReader.java
+++ app/src/main/java/com/example/myfirstapp/CompassAzimuthReader.java
@@ -14,6 +14,7 @@ public class CompassAzimuthReader implements SensorEventListener {
     private CompassAzimuthReaderDelegate delegate;
 
     private boolean ignoreRotationVector;
+    private float lowPassFilterAlpha;
 
     private Sensor rotationVectorSensor;
     private Sensor accelerometerSensor;
@@ -22,13 +23,21 @@ public class CompassAzimuthReader implements SensorEventListener {
     private float[] rotationMatrix = new float[9];
     private float[] orientation = new float[3];
 
-    private float[] lastAccelerometer = new float[3];
-    private float[] lastMagnetometer = new float[3];
+    private float RTmp[] = new float[9];
+    private float Rot[] = new float[9];
+    private float I[] = new float[9];
+    private float results[] = new float[3];
 
-    public CompassAzimuthReader(SensorManager sensorManager, CompassAzimuthReaderDelegate delegate, boolean ignoreRotationVector) {
+    private float[] gravSensorVals;
+    private float[] magSensorVals;
+
+
+
+    public CompassAzimuthReader(SensorManager sensorManager, CompassAzimuthReaderDelegate delegate, boolean ignoreRotationVector, float lowPassFilterAlpha) {
         this.sensorManager = sensorManager;
         this.delegate = delegate;
         this.ignoreRotationVector = ignoreRotationVector;
+        this.lowPassFilterAlpha = lowPassFilterAlpha;
     }
 
     public void start() throws IOException {
@@ -82,16 +91,26 @@ public class CompassAzimuthReader implements SensorEventListener {
         }
 
         if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-            System.arraycopy(event.values, 0, lastAccelerometer, 0, event.values.length);
+            gravSensorVals = lowPass(event.values.clone(), gravSensorVals);
         } else if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
-            System.arraycopy(event.values, 0, lastMagnetometer, 0, event.values.length);
+            magSensorVals = lowPass(event.values.clone(), magSensorVals);
         }
 
-        if (lastAccelerometer != null && lastMagnetometer != null) {
-            SensorManager.getRotationMatrix(rotationMatrix, null, lastAccelerometer, lastMagnetometer);
-            SensorManager.getOrientation(rotationMatrix, orientation);
-            azimuth = (Math.toDegrees(SensorManager.getOrientation(rotationMatrix, orientation)[0]) + 360) % 360;
+        if (gravSensorVals != null && magSensorVals != null) {
+            SensorManager.getRotationMatrix(RTmp, I, gravSensorVals, magSensorVals);
+            SensorManager.remapCoordinateSystem(RTmp, SensorManager.AXIS_X, SensorManager.AXIS_MINUS_Y, Rot);
+            SensorManager.getOrientation(Rot, results);
+            azimuth = (((results[0]*180)/Math.PI)+180);
         }
+    }
+
+    private float[] lowPass( float[] input, float[] output ) {
+        if ( output == null ) return input;
+
+        for ( int i=0; i<input.length; i++ ) {
+            output[i] = output[i] + lowPassFilterAlpha * (input[i] - output[i]);
+        }
+        return output;
     }
 
     @Override
