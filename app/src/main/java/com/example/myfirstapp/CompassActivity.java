c6e98ce21b6abec19390e387f8e18f78064bab8b
Oscar Fridh <oscar.fridh.mkk@gmail.com>
2020-03-29 09:03:47 +0200

    Refactored Activity

--- app/src/main/java/com/example/myfirstapp/CompassActivity.java
+++ app/src/main/java/com/example/myfirstapp/CompassActivity.java
@@ -11,10 +11,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class CompassActivity extends AppCompatActivity implements CompassAzimuthReaderDelegate {
 
-    ImageView compass_img;
-    TextView txt_compass;
-    CompassAzimuthReader compassAzimuthReader;
-
+    private ImageView compass_img;
+    private TextView txt_compass;
+    private CompassAzimuthReader compassAzimuthReader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,50 +23,32 @@ public class CompassActivity extends AppCompatActivity implements CompassAzimuth
         compassAzimuthReader = new CompassAzimuthReader((SensorManager) getSystemService(SENSOR_SERVICE), this);
         compass_img = (ImageView) findViewById(R.id.img_compass);
         txt_compass = (TextView) findViewById(R.id.txt_azimuth);
-
-        if (!compassAzimuthReader.start()) {
-            noSensorsAlert();
-        }
     }
 
     @Override
     public void updateCompass(int mAzimuth) {
         compass_img.setRotation(-mAzimuth);
-
-        String where = "NW";
-
-        if (mAzimuth >= 350 || mAzimuth <= 10)
-            where = "N";
-        if (mAzimuth < 350 && mAzimuth > 280)
-            where = "NW";
-        if (mAzimuth <= 280 && mAzimuth > 260)
-            where = "W";
-        if (mAzimuth <= 260 && mAzimuth > 190)
-            where = "SW";
-        if (mAzimuth <= 190 && mAzimuth > 170)
-            where = "S";
-        if (mAzimuth <= 170 && mAzimuth > 100)
-            where = "SE";
-        if (mAzimuth <= 100 && mAzimuth > 80)
-            where = "E";
-        if (mAzimuth <= 80 && mAzimuth > 10)
-            where = "NE";
-
-
-        txt_compass.setText(mAzimuth + "° " + where);
+        txt_compass.setText(mAzimuth + "° " + heading(mAzimuth));
     }
 
-
-    public void noSensorsAlert() {
-        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
-        alertDialog.setMessage("Your device doesn't support the Compass.")
-                .setCancelable(false)
-                .setNegativeButton("Close", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        finish();
-                    }
-                });
-        alertDialog.show();
+    private String heading(int mAzimuth) {
+        if (mAzimuth >= 350 || mAzimuth <= 10) {
+            return "N";
+        } else if (mAzimuth < 350 && mAzimuth > 280) {
+            return "NW";
+        } else if (mAzimuth <= 280 && mAzimuth > 260) {
+            return "W";
+        } else if (mAzimuth <= 260 && mAzimuth > 190) {
+            return "SW";
+        } else if (mAzimuth <= 190 && mAzimuth > 170) {
+            return "S";
+        } else if (mAzimuth <= 170 && mAzimuth > 100) {
+            return "SE";
+        } else if (mAzimuth <= 100 && mAzimuth > 80) {
+            return "E";
+        } else {
+            return "NE";
+        }
     }
 
     @Override
@@ -84,4 +65,15 @@ public class CompassActivity extends AppCompatActivity implements CompassAzimuth
         }
     }
 
+    private void noSensorsAlert() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+        alertDialog.setMessage("Your device doesn't support the Compass.")
+                .setCancelable(false)
+                .setNegativeButton("Close", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        finish();
+                    }
+                });
+        alertDialog.show();
+    }
 }
