5bad81405c5a7c25e7d60f7a2bbdf3e0bebc30a2
Oscar Fridh <oscar.fridh.mkk@gmail.com>
2020-03-30 10:03:48 +0200

    Implemented the accelerometers view

--- app/src/main/java/com/example/myfirstapp/AccelerometersActivity.java
+++ app/src/main/java/com/example/myfirstapp/AccelerometersActivity.java
@@ -2,13 +2,65 @@ package com.example.myfirstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class AccelerometersActivity extends AppCompatActivity {
+public class AccelerometersActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager sensorManager;
+    private Sensor sensor;
+
+    TextView textViewX;
+    TextView textViewY;
+    TextView textViewZ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accelerometers);
+
+        textViewX = findViewById(R.id.x);
+        textViewY = findViewById(R.id.y);
+        textViewZ = findViewById(R.id.z);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
     }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        textViewX.setText("X: " + Math.round(event.values[0]));
+        textViewY.setText("Y: " + Math.round(event.values[1]));
+        textViewZ.setText("Z: " + Math.round(event.values[2]));
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stop();
+    }
+
+    private void stop() {
+        sensorManager.unregisterListener(this, sensor);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        start();
+    }
+
+    private void start() {
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_UI);
+    }
+
 }
